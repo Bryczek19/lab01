3bb9621008bb6de5e902a05532015340059409e8
Bryczek19 <Konrad.bryk@microsoft.wsei.edu.pl>
2025-11-02 22:33:37 +0100

    lab4.441

--- src/components/RatingBar.jsx
+++ src/components/RatingBar.jsx
@@ -1,6 +1,7 @@
-export default function RatingBar({ rate }) {
+export default function RatingBar({ rate = 0 }) {
   const totalStars = 10;
   const stars = [];
+
   for (let i = 1; i <= totalStars; i++) {
     stars.push(
       <span key={i} style={{ color: i <= rate ? "#f5b301" : "#ccc" }}>
@@ -8,5 +9,6 @@ export default function RatingBar({ rate }) {
       </span>
     );
   }
+
   return <div style={{ fontSize: "1.2rem" }}>{stars}</div>;
 }
